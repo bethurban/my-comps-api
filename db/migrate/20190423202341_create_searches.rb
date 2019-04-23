0d84a53b24336db3dd11b5c9b742d2c75c312758
bethurban <beth.urban@gmail.com>
2019-04-23 16:27:58 -0400

    Add Search model

---
+++ db/migrate/20190423202341_create_searches.rb
@@ -0,0 +1,10 @@
+class CreateSearches < ActiveRecord::Migration[5.2]
+  def change
+    create_table :searches do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
