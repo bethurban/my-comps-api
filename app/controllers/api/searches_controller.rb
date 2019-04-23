3b7d843d04647d5ff14bdaaae61cc0dcad5d124f
bethurban <beth.urban@gmail.com>
2019-04-23 17:04:31 -0400

    Create SearchesController and fill out routes

---
+++ app/controllers/api/searches_controller.rb
@@ -0,0 +1,47 @@
+class Api::SearchesController < ApplicationController
+
+  before_action :set_search, only: [:show, :edit, :destroy]
+
+  def index
+    render json: Search.all
+  end
+
+  def create
+    search = Search.new(search_params)
+    if search.save
+      render json: search
+    else
+      render json: { message: search.errors }, status: 400
+    end
+  end
+
+  def show
+    render json: @search
+  end
+
+  def update
+    if @search.update(search_params)
+      render json: @search
+    else
+      render json: { message: @search.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @search.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to destroy this search" }, status: 400
+    end
+  end
+
+  private
+
+    def set_search
+      @search = Search.find_by(id: params[:id])
+    end
+
+    def search_params
+      params.require(:search).permit(:name, :address)
+    end
+end
