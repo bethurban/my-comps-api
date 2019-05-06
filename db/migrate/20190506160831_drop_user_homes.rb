a2186f57f15efa8b9ef9f98f323920c40e095f2b
bethurban <beth.urban@gmail.com>
2019-05-06 12:13:31 -0400

    Delete unnecessary UserHomes join table. Create User model and add associations.

---
+++ db/migrate/20190506160831_drop_user_homes.rb
@@ -0,0 +1,5 @@
+class DropUserHomes < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :user_homes
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_155612) do
+ActiveRecord::Schema.define(version: 2019_05_06_160941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_155612) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "citystate"
-  end
-
-  create_table "user_homes", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "home_id"
   end
 
   create_table "users", force: :cascade do |t|
